2d2f1b80e91bbea6bce5e2988821743e96efb2a5
Ibraheem AJ <gai10@uni.brighton.ac.uk>
2015-04-18 14:05:19 +0100

    Fix

--- QuoteBook/AppDelegate.h
+++ QuoteBook/AppDelegate.h
@@ -24,14 +24,28 @@
 @end
 
 // Feature list
+// - Add the new StandS4 API to the server
 // 1. Add notification features for early morning daily quote
 // 2. Implement notification center widget
 // 3. Add apple watch target
-// 4. Add a menu { offer customisation }
+// 4. Add a menu { allow user change the colors }
 // 5. Add advertisements to the app
 // 6. Create the mac version
 // 7. Add a share feature for the quotes
+// 8. Integrate beautiful adverts in the app
+// - Allow people add their own quotes { People can upvote & downvote quotes just like stack overflow, User generated content + My generated content 🙌🏽🙌🏽🙌🏽 }
+
 
 // Bugs list
-// -- Add support for swift 1.1
+// -- Fix the bug on the server side, preventing cron jobs from executing with results
+// -- Allow text re-size to accomodate for long quotes
 // -- Alert the user when there is no network connection
+// -- server side, write a script to prevent duplicate quotes
+=======
+// 1. Add notification features for early morning daily quote
+// 2. Implement notification center widget
+// 3. Add apple watch target
+
+// Bugs list
+// -- No Bugs --
+
